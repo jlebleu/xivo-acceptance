4c9d1a118f2736d8dd978f6bff7c7162daa80be2
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-04-09 15:34:48 -0400

    fix ule helper to use restapi responses

--- xivo_acceptance/helpers/user_line_extension_helper.py
+++ xivo_acceptance/helpers/user_line_extension_helper.py
@@ -35,7 +35,7 @@ def delete_user_line_extension_voicemail(firstname, lastname, context=None, exte
 def delete_user_line_extension_with_firstname_lastname(firstname, lastname):
     user = user_helper.find_by_firstname_lastname(firstname, lastname)
     if user:
-        delete_user_line_extension_with_user_id(user.id)
+        delete_user_line_extension_with_user_id(user['id'])
 
 
 def delete_sccp_lines_with_exten(exten, context):
@@ -58,7 +58,7 @@ def delete_user_line_extension_with_user_id(user_id):
     if extension_id:
         extension_helper.delete_extension(extension_id)
 
-    user_helper.delete_user(user.id)
+    user_helper.delete_user(user['id'])
 
 
 def add_or_replace_user(data_dict):
